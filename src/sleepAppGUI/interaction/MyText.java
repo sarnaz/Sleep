6f7bd39f003fbe931312ab31e4410cd5add29b02
bookla <booklailert@gmail.com>
2022-03-26 16:35:21 +0000

    Allow for setting type face and colour

--- src/sleepAppGUI/interaction/MyText.java
+++ src/sleepAppGUI/interaction/MyText.java
@@ -6,17 +6,37 @@ public class MyText extends VObject
 {
     private String text;
     private String font = "Century Gothic";
+    private final Color colour;
+    private int style = Font.PLAIN;
 
-    public MyText(Page page, int[] coordinates1, int[] coordinates2, String ttext)
+    public MyText(Page page, int[] coordinates1, int[] coordinates2, String text)
     {
         super(page, coordinates1, coordinates2, true);
-        text = ttext;
+        this.text = text;
+        colour = Color.BLACK;
+    }
+
+    public MyText(Page page, int[] coordinates1, int[] coordinates2, String text, int[] textColour)
+    {
+        super(page, coordinates1, coordinates2, true);
+        this.text = text;
+        colour = Color.getHSBColor(textColour[0], textColour[1], textColour[2]);
+    }
+
+    public MyText(Page page, int[] coordinates1, int[] coordinates2, String text, int[] textColour, String fontName, int style)
+    {
+        super(page, coordinates1, coordinates2, true);
+        this.text = text;
+        this.font = fontName;
+        this.style = style;
+        colour = Color.getHSBColor(textColour[0], textColour[1], textColour[2]);
     }
 
     @Override
     public void paint(Graphics g)
     {
-        g.setFont(new Font(font, Font.PLAIN ,corner2[1] - corner1[1]));
+        g.setFont(new Font(font, style ,corner2[1] - corner1[1]));
+        g.setColor(colour);
         g.drawString(text, corner1[0], corner1[1]);
     }
 
