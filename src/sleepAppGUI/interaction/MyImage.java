2444897af7010db9b469ac422f86b751c9ed1718
bookla <booklailert@gmail.com>
2022-03-26 16:41:08 +0000

    Changed internal initializer signature. (Does not affect external use apart from code inspection) Allow for custom file extension

--- src/sleepAppGUI/interaction/MyImage.java
+++ src/sleepAppGUI/interaction/MyImage.java
@@ -8,19 +8,27 @@ import java.io.IOException;
 public class MyImage extends VObject
 {
     String image;
+    String extension;
 
     public MyImage(Page page, int[] coordinates1, int[] coordinates2, String imageName, boolean defaultVisible)
     {
         super(page, coordinates1, coordinates2, defaultVisible);
         image = imageName;
+        extension = ".png";
+    }
+
+    public MyImage(Page page, int[] coordinates1, int[] coordinates2, String imageName, String fileExtension, boolean defaultVisible) {
+        super(page, coordinates1, coordinates2, defaultVisible);
+        image = imageName;
+        extension = fileExtension;
     }
 
     public void paint(Graphics g)
     {
         try
         {
-            g.drawImage(ImageIO.read(new File("assets/"+image+".png")), corner1[0], corner1[1], corner2[0] - corner1[0], corner2[1] - corner1[1], null);
+            g.drawImage(ImageIO.read(new File("assets/" + image + extension)), corner1[0], corner1[1], corner2[0] - corner1[0], corner2[1] - corner1[1], null);
         }
-        catch(IOException f) { System.out.println(image+" couldn't be found"); }
+        catch(IOException f) { System.out.println(image + " couldn't be found"); }
     }
 }
