cd0228701797e1bb8c1d6326126897fe824d8546
bookla <booklailert@gmail.com>
2022-03-26 16:41:33 +0000

    Updated sign in page

--- src/sleepAppGUI/interaction/Page.java
+++ src/sleepAppGUI/interaction/Page.java
@@ -176,7 +176,7 @@ public class Page
         */
 
         // Create the pages here nice color: 0xC7EFF9
-        Page username_password_initial = new Page(2, main, new Color(0xC7EFF9));
+        Page username_password_initial = new Page(2, main, new Color(0x6D3FB2));
         Page more_info_page = new Page(3, main, new Color(0xC7EFF9));
         // Page account_created = new Page(4, main, new Color(0xC7EFF9));
         Page graph_visual = new Page(5, main, new Color(0xC7EFF9));
@@ -293,25 +293,25 @@ public class Page
     private static void setUpSignInPage(Main main, Page username_password_initial, Page nextPage) {
         // ADD THINGS TO FIRST PAGE
         // Adds logo
-        MyImage logo = new MyImage(username_password_initial, new int[] {185, 15}, new int[] {615, 160}, "logo", true);
-        // Adds the box to go behind input fields
-        MyImage input_frame = new MyImage(username_password_initial, new int[] {275, 170}, new int[] {525, 400}, "box_behind", true);
-        // Adds the username input box on first page
-        MyText usernameText = new MyText(username_password_initial, new int[] {350, 200}, new int[] {365, 220}, "Username:");
-        MyTextField usernameInput = new MyTextField(main, username_password_initial, new int[] {300, 210}, new int[] {500, 235});
-        // Adds the password boxes
-        MyText password = new MyText(username_password_initial, new int[] {350, 265}, new int[] {365, 285}, "Password:");
-        MyPasswordField passwordInput1 = new MyPasswordField(main, username_password_initial, new int[] {300, 275}, new int[] {500, 300});
-        MyText passwordReenter = new MyText(username_password_initial, new int[] {305, 330}, new int[] {320, 350}, "Re-enter Password:");
-        MyPasswordField passwordInput2 = new MyPasswordField(main, username_password_initial, new int[] {300, 340}, new int[] {500, 365});
+        new MyImage(username_password_initial, new int[] {0, 1}, new int[] {800, 600}, "first-page", ".jpg", true);
 
-        MyImage openEye = new MyImage(username_password_initial, new int[]{450, 250}, new int[]{470, 270}, "openEye", false);
-        MyImage closedEye = new MyImage(username_password_initial, new int[]{450, 250}, new int[]{470, 270}, "closedEye", true);
-        
+        new MyText(username_password_initial, new int[] {400, 190}, new int[] {440, 215}, "Username:", new int[] {0, 0, 1}, "Helvetica", Font.BOLD);
+        MyTextField usernameInput = new MyTextField(main, username_password_initial, new int[] {400, 205}, new int[] {750, 235});
+
+        // Adds the password boxes
+        new MyText(username_password_initial, new int[] {400, 280}, new int[] {440, 305}, "Password:", new int[] {0, 0, 1}, "Helvetica", Font.BOLD);
+        MyPasswordField passwordInput1 = new MyPasswordField(main, username_password_initial, new int[] {400, 295}, new int[] {750, 325});
+
+        new MyText(username_password_initial, new int[] {400, 370}, new int[] {440, 395}, "Re-enter Password:", new int[] {0, 0, 1}, "Helvetica", Font.BOLD);
+        MyPasswordField passwordInput2 = new MyPasswordField(main, username_password_initial, new int[] {400, 385}, new int[] {750, 415});
+
+        MyImage openEye = new MyImage(username_password_initial, new int[]{730, 250}, new int[]{750, 270}, "openEye", false);
+        MyImage closedEye = new MyImage(username_password_initial, new int[]{730, 250}, new int[]{750, 270}, "closedEye", true);
+
         username_password_initial.pushToFront(openEye);
         username_password_initial.pushToFront(closedEye);
 
-        MyButton toggleShowPasswordButton = new MyButton(username_password_initial, "show password", new int[] {450, 250}, new int[] {470, 270}, null)
+        new MyButton(username_password_initial, "show password", new int[] {730, 250}, new int[] {750, 270}, null)
         {
         	public void isClicked()
         	{
@@ -324,7 +324,7 @@ public class Page
         };
 
         // Add next button
-        MyButton nextButton = new MyButton(username_password_initial, "next", new int[] {360, 400}, new int[] {440, 435}, "next")
+        new MyButton(username_password_initial, "next", new int[] {680, 420}, new int[] {750, 470}, "next-button")
         {
             public void isClicked()
             {
