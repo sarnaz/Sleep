86bcf3c5f4e0fc976a6ebb80de9aaaa5d6fbb168
bookla <booklailert@gmail.com>
2022-03-31 13:53:19 +0100

    fix: fixed MyText's `putText` function

--- src/sleepAppGUI/interaction/MyText.java
+++ src/sleepAppGUI/interaction/MyText.java
@@ -4,16 +4,19 @@ import java.awt.*;
 
 public class MyText extends VObject
 {
+    public final static String defaultFont = "Century Gothic";
+    public final static int[] defaultColour = new int[] {0, 0, 0};
+    public final static int defaultStyle = Font.PLAIN;
+
     private String text;
-    private String font = "Century Gothic";
-    private final Color colour;
-    private int style = Font.PLAIN;
+    private String font = defaultFont;
+    private Color colour = Color.getHSBColor(defaultColour[0], defaultColour[1], defaultColour[2]);
+    private int style = defaultStyle;
 
     public MyText(Page page, int[] coordinates1, int[] coordinates2, String text)
     {
         super(page, coordinates1, coordinates2, true);
         this.text = text;
-        colour = Color.BLACK;
     }
 
     public MyText(Page page, int[] coordinates1, int[] coordinates2, String text, int[] textColour)
@@ -32,14 +35,14 @@ public class MyText extends VObject
         colour = Color.getHSBColor(textColour[0], textColour[1], textColour[2]);
     }
 
-    public MyText putText(Page page, int[] coordinates1, int[] size, String text, int[] textColour, String fontName, int style) {
+    public static MyText putText(Page page, int[] coordinates1, int[] size, String text, int[] textColour, String fontName, int style) {
         final int[] coordinates2 = new int[] {coordinates1[0] + size[0], coordinates1[1] + size[1]};
 
         return new MyText(page, coordinates1, coordinates2, text, textColour, fontName, style);
     }
 
-    public MyText putText(Page page, int[] coordinates1, int[] size, String text) {
-        return putText(page, coordinates1, size, text, new int[] {0, 0, 0}, font, style);
+    public static MyText putText(Page page, int[] coordinates1, int[] size, String text) {
+        return putText(page, coordinates1, size, text, defaultColour, defaultFont, defaultStyle);
     }
 
     @Override
