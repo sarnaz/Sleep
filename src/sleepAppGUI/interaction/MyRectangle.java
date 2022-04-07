9fd5dfa41c54c49b7ef12c6df86554a2e9f399ef
Book Lailert <pl673@bath.ac.uk>
2022-04-07 11:00:43 +0100

    Complete MyRectangle class

--- src/sleepAppGUI/interaction/MyRectangle.java
+++ src/sleepAppGUI/interaction/MyRectangle.java
@@ -5,13 +5,32 @@ import java.awt.*;
 
 public class MyRectangle extends JFrame {
 
-    public MyRectangle(int[] size) {
+    private final int width;
+    private final int height;
+    private final int x;
+    private final int y;
+    private int arcX = 0;
+    private int arcY = 0;
 
-        setSize(size[0], size[1]);
+    public MyRectangle(int[] coordinates, int[] size) {
+        this.width = size[0];
+        this.height = size[1];
+        this.x = coordinates[0];
+        this.y = coordinates[1];
+    }
+
+    public MyRectangle(int[] coordinates, int[] size, int cornerRadius) {
+        this(coordinates, size, new int[] {cornerRadius, cornerRadius});
+    }
+
+    public MyRectangle(int[] coordinates, int[] size, int[] cornerRadius) {
+        this(coordinates, size);
+        this.arcX = cornerRadius[0];
+        this.arcY = cornerRadius[1];
     }
 
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRoundRect(10, 50, 150, 150, 50, 30); // to draw a rounded rectangle.
+        g2d.drawRoundRect(x, y, width, height, arcX, arcY);
     }
 }
