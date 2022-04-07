302e678788c89e66797f8843576ae0d7dd55f892
Book Lailert <pl673@bath.ac.uk>
2022-04-07 11:19:01 +0100

    MyRectangle can now draw colour filled rectangle

--- src/sleepAppGUI/interaction/MyRectangle.java
+++ src/sleepAppGUI/interaction/MyRectangle.java
@@ -1,9 +1,8 @@
 package sleepAppGUI.interaction;
 
-import javax.swing.*;
 import java.awt.*;
 
-public class MyRectangle extends JFrame {
+public class MyRectangle extends VObject {
 
     private final int width;
     private final int height;
@@ -11,26 +10,39 @@ public class MyRectangle extends JFrame {
     private final int y;
     private int arcX = 0;
     private int arcY = 0;
+    private Color color;
 
-    public MyRectangle(int[] coordinates, int[] size) {
+    private static int[] getSecondCoordinates(int[] coordinates, int[] size) {
+        return new int[] {coordinates[0] + size[0], coordinates[1] + size[1]};
+    }
+
+    public MyRectangle(Page page, int[] coordinates, int[] size, Color color) {
+        super(page, coordinates, getSecondCoordinates(coordinates, size), true);
         this.width = size[0];
         this.height = size[1];
         this.x = coordinates[0];
         this.y = coordinates[1];
+        this.color = color;
     }
 
-    public MyRectangle(int[] coordinates, int[] size, int cornerRadius) {
-        this(coordinates, size, new int[] {cornerRadius, cornerRadius});
+    public MyRectangle(Page page, int[] coordinates, int[] size, int cornerRadius, Color color) {
+        this(page, coordinates, size, new int[] {cornerRadius, cornerRadius}, color);
     }
 
-    public MyRectangle(int[] coordinates, int[] size, int[] cornerRadius) {
-        this(coordinates, size);
+    public MyRectangle(Page page, int[] coordinates, int[] size, int[] cornerRadius, Color color) {
+        this(page, coordinates, size, color);
         this.arcX = cornerRadius[0];
         this.arcY = cornerRadius[1];
+        this.color = color;
     }
 
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRoundRect(x, y, width, height, arcX, arcY);
+        if (this.color != null) {
+            g2d.setColor(this.color);
+            g2d.fillRoundRect(x, y, width, height, arcX, arcY);
+        } else {
+            g2d.drawRoundRect(x, y, width, height, arcX, arcY);
+        }
     }
 }
