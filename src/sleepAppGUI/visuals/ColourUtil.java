c1a4e14735957be43ab112c5652d2b4fba748a98
Book Lailert <pl673@bath.ac.uk>
2022-04-27 16:06:24 +0100

    Record for colour palette

---
+++ src/sleepAppGUI/visuals/ColourUtil.java
@@ -0,0 +1,11 @@
+package sleepAppGUI.visuals;
+
+import java.awt.*;
+
+public record ColourUtil() {
+
+    public static Color foregroundColour = new Color(121, 61, 187);
+    public static Color backgroundColour = new Color(27, 5, 59);
+    public static Color accentColour = new Color(102, 24, 151);
+
+}
