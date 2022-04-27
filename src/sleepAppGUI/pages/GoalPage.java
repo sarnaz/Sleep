17f9af99d91197df285160691b3c68d46f2ffc9f
Book Lailert <pl673@bath.ac.uk>
2022-04-27 22:50:34 +0100

    Updated `GoalPage`

--- src/sleepAppGUI/pages/GoalPage.java
+++ src/sleepAppGUI/pages/GoalPage.java
@@ -3,7 +3,9 @@ package sleepAppGUI.pages;
 import sleepAppDatabase.Database;
 import sleepAppGUI.interaction.*;
 import sleepAppGUI.pages.goals.*;
+import sleepAppGUI.visuals.ColourUtil;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -19,77 +21,112 @@ public class GoalPage extends UIViewPage {
         calendar.setTime(new Date());
         Object Goal_array[][] = Database.getGoalData();
         Object DailyQData_array[][] = Database.getDataForDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
-        MyImage layout = new MyImage(page, new int[] {0, 20}, new int[] {800, 476}, "goal_layout", true);
 
-        // example of how to display goal
-        MyText watergoal = new MyText(page, new int[] {110, 200}, new int[] {320, 230}, DailyQData_array[1][2] + " / " + Goal_array[1][0]);
-        MyText sleepgoal = new MyText(page, new int[] {280, 200}, new int[] {320, 230}, DailyQData_array[1][3] + " / " + Goal_array[1][1]);
-        MyText stressgoal = new MyText(page, new int[] {540, 290}, new int[] {740, 320}, DailyQData_array[1][6] + " / " + Goal_array[1][5]);
-        MyText exercisegoal = new MyText(page, new int[] {110, 380}, new int[] {520, 410}, DailyQData_array[1][8] + " / " + Goal_array[1][2]);
-        MyText coffeegoal = new MyText(page, new int[] {525, 410}, new int[] {640, 435}, DailyQData_array[1][1] + " / " + Goal_array[1][7]);
-        MyText teagoal = new MyText(page, new int[] {433, 410}, new int[] {540, 435}, DailyQData_array[1][1] + " / " + Goal_array[1][6]);
-        MyText energydrinksgoal = new MyText(page, new int[] {613, 410}, new int[] {740, 435}, DailyQData_array[1][1] + " / " + Goal_array[1][8]);
-        MyText screentimegoal = new MyText(page, new int[] {540, 180}, new int[] {740, 210}, DailyQData_array[1][7] + " / " + Goal_array[1][4]);
-        MyText alcoholgoal = new MyText(page, new int[] {280, 380}, new int[] {320, 410}, DailyQData_array[1][0] + " / " + Goal_array[1][3]);
+        new MyRectangle(page, new int[] {40, 21}, new int[] {720, 455}, 40, ColourUtil.lightForegroundColour);
+        new MyRectangle(page, new int[] {40, 21}, new int[] {720, 80}, 40, ColourUtil.foregroundColour);
+        MyText.putText(page, new int[] {56, 75}, 40, "Goals", Color.white, "Helvetica", Font.BOLD);
+
+        new MyRectangle(page, new int[] {70, 127}, new int[] {152, 140}, 20, Color.white);
+        new MyRectangle(page, new int[] {247, 125}, new int[] {152, 140}, 20, Color.white);
+        new MyRectangle(page, new int[] {435, 243}, new int[] {290, 105}, 20, Color.white);
+        new MyRectangle(page, new int[] {70, 306}, new int[] {152, 140}, 20, Color.white);
+        new MyRectangle(page, new int[] {435, 360}, new int[] {290, 105}, 20, Color.white);
+        new MyRectangle(page, new int[] {247, 306}, new int[] {152, 140}, 20, Color.white);
+        new MyRectangle(page, new int[] {435, 123}, new int[] {290, 105}, 20, Color.white);
+        // Water
+        MyImage.putImage(page, new int[] {122, 160}, 50, "glass_icon");
+        MyText.putTextCentred(page, new int[] {145, 150}, 18, "Water", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {145, 200}, 30, DailyQData_array[1][2] + " / " + Goal_array[1][0], Color.black);
+        // Sleep
+        MyImage.putImage(page, new int[] {295, 172}, 50, "bed_icon");
+        MyText.putTextCentred(page, new int[] {323, 150}, 18, "Sleep", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {323, 200}, 30, DailyQData_array[1][3] + " / " + Goal_array[1][1], Color.black);
+        // Stress
+        MyImage.putImage(page, new int[] {559, 277}, 40, "stress_icon");
+        MyText.putTextCentred(page, new int[] {579, 267}, 18, "Stress", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {579, 310}, 30, DailyQData_array[1][6] + " / " + Goal_array[1][5], Color.black);
+        // Exercise
+        MyImage.putImage(page, new int[] {117, 350}, 60, "exercise_icon");
+        MyText.putTextCentred(page, new int[] {145, 330}, 18, "Exercise", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {145, 390}, 30, DailyQData_array[1][8] + " / " + Goal_array[1][2], Color.black);
+        // Tea
+        MyImage.putImage(page, new int[] {460, 390}, 210, "caffeine_icon");
+        MyText.putTextCentred(page, new int[] {570, 384}, 14, "Tea", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {570, 420}, 18, DailyQData_array[1][1] + " / " + Goal_array[1][7], Color.black);
+        // Coffee
+        MyText.putTextCentred(page, new int[] {477, 384}, 14, "Coffee", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {477, 420},18, DailyQData_array[1][1] + " / " + Goal_array[1][6], Color.black);
+        // Energy Drink
+        MyText.putTextCentred(page, new int[] {658, 384}, 14, "Energy Drink", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {658, 420}, 18, DailyQData_array[1][1] + " / " + Goal_array[1][8], Color.black);
+        // Screen Time
+        MyImage.putImage(page, new int[] {555, 157}, 60, "computer_icon");
+        MyText.putTextCentred(page, new int[] {585, 147}, 18, "Screen Time", Color.black, "Helvetica", Font.BOLD);
+        MyText.putTextCentred(page, new int[] {585, 190}, 30, DailyQData_array[1][7] + " / " + Goal_array[1][4], Color.black);
+        // Alcohol
+        MyImage.putImage(page, new int[] {303, 340}, 50, "alcohol_icon");
+        MyText.putTextCentred(page, new int[] {325, 330}, 18, "Alcohol", Color.black, "Helvetica", Font.BOLD);
+
+        MyText.putTextCentred(page, new int[] {325, 380}, 30, DailyQData_array[1][0] + " / " + Goal_array[1][3], Color.black);
 
         // buttons to edit goals
-        MyButton EditWaterGoal = new MyButton(page, "editWater", new int[]{70, 248}, new int[]{222, 271}, "edit_goal_1"){
+        new MyButton(page, "editWater", new int[]{70, 248}, new int[]{222, 271}, "edit_goal_1"){
             public void isClicked()
             {
                 GoalPage.this.push(new WaterGoal());
                 System.out.println("Edit Water Goal Page");
             }
         };
-        MyButton EditSleepGoal = new MyButton(page, "editSleep", new int[]{247, 248}, new int[]{399, 271}, "edit_goal_1"){
+        new MyButton(page, "editSleep", new int[]{247, 248}, new int[]{399, 271}, "edit_goal_1"){
             public void isClicked()
             {
                 GoalPage.this.push(new SleepGoal());
                 System.out.println("Edit Sleep Goal Page");
             }
         };
-        MyButton EditExerciseGoal = new MyButton(page, "editExercise", new int[]{70, 427}, new int[]{222, 450}, "edit_goal_1"){
+        new MyButton(page, "editExercise", new int[]{70, 427}, new int[]{222, 450}, "edit_goal_1"){
             public void isClicked()
             {
                 GoalPage.this.push(new ExerciseGoal());
                 System.out.println("Edit Exercise Goal Page");
             }
         };
-        MyButton EditAlcoholGoal = new MyButton(page, "editAlcohol", new int[]{247, 427}, new int[]{399, 450}, "edit_goal_1"){
+        new MyButton(page, "editAlcohol", new int[]{247, 427}, new int[]{399, 450}, "edit_goal_1"){
             public void isClicked()
             {
                 GoalPage.this.push(new AlcoholGoal());
                 System.out.println("Edit Alcohol Goal Page");
             }
         };
-        MyButton EditScreenTimeGoal = new MyButton(page, "editScreenTime", new int[]{709, 113}, new int[]{731, 196}, "edit_goal_2"){
+        new MyButton(page, "editScreenTime", new Rectangle(708, 123, 23,105), "edit_goal_2"){
             public void isClicked()
             {
                 GoalPage.this.push(new ScreenGoal());
                 System.out.println("Edit Screen Time Goal Page");
             }
         };
-        MyButton EditStressGoal = new MyButton(page, "editStress", new int[]{709, 220}, new int[]{731, 321}, "edit_goal_2"){
+        new MyButton(page, "editStress", new Rectangle(708, 243, 23,105), "edit_goal_2"){
             public void isClicked()
             {
                 GoalPage.this.push(new StressGoal());
                 System.out.println("Edit Stress Goal Page");
             }
         };
-        MyButton EditCaffeineGoal = new MyButton(page, "editCaffeine", new int[]{709, 345}, new int[]{731, 450}, "edit_goal_2"){
+        new MyButton(page, "editCaffeine", new Rectangle(708, 360, 23,105), "edit_goal_2"){
             public void isClicked()
             {
                 GoalPage.this.push(new CaffeineGoal());
                 System.out.println("Edit Caffeine Goal Page");
             }
         };
-        MyButton backButton = new MyButton(page, "back", new int[] {100, 490}, new int[] {190, 520}, "back_button") {
+        new MyButton(page, "back", new int[] {100, 490}, new int[] {190, 520}, "back_button") {
             public void isClicked()
             {
                 GoalPage.this.push(new HomePage());
                 System.out.println("Home Page");
             }
         };
-        MyButton suggestionsButton = new MyButton(page, "suggestions", new int[] {520, 490}, new int[] {662, 520}, "suggestions_button") {
+        new MyButton(page, "suggestions", new int[] {520, 490}, new int[] {662, 520}, "suggestions_button") {
             public void isClicked()
             {
                 GoalPage.this.push(new SuggestionPage());
