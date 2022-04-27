7a379232889ca1156e65dc0e9d9c56507e8d04b7
Book Lailert <pl673@bath.ac.uk>
2022-04-27 22:56:08 +0100

    Updated `GoalSet` (common style for setting goals)

--- src/sleepAppGUI/pages/goals/GoalSet.java
+++ src/sleepAppGUI/pages/goals/GoalSet.java
@@ -2,10 +2,17 @@ package sleepAppGUI.pages.goals;
 
 import sleepAppGUI.interaction.*;
 import sleepAppGUI.pages.GoalPage;
+import sleepAppGUI.visuals.ColourUtil;
+
+import java.awt.*;
 
 abstract public class GoalSet extends UIViewPage {
     @Override
     protected void setUp(Page page) {
+
+        MyImage.putImage(page, new int[] {300, 20}, 200, "logo");
+        new MyRectangle(page, new int[] {80, 100}, new int[] {640, 370}, 50, Color.white);
+        new MyRectangle(page, new int[] {80, 100}, new int[] {640, 90}, 50, ColourUtil.foregroundColour);
         MyButton backButton = new MyButton(page, "back", new int[]{240, 525}, new int[]{330, 555}, "back_button") {
             public void isClicked() {
                 GoalSet.this.push(new GoalPage());
