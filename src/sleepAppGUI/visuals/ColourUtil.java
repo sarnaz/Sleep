a73eb693f56efbd822e24384a76dd92419bdcdb1
Book Lailert <pl673@bath.ac.uk>
2022-04-27 21:55:48 +0100

    Added additional colours to the colour palette

--- src/sleepAppGUI/visuals/ColourUtil.java
+++ src/sleepAppGUI/visuals/ColourUtil.java
@@ -7,5 +7,8 @@ public record ColourUtil() {
     public static Color foregroundColour = new Color(121, 61, 187);
     public static Color backgroundColour = new Color(27, 5, 59);
     public static Color accentColour = new Color(102, 24, 151);
+    public static Color darkForegroundColour = new Color(101, 46, 162);
+
+    public static Color lightForegroundColour = new Color(187, 124, 255);
 
 }
