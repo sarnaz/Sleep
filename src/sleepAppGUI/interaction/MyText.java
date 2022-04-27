9de8d7f42b93dbee11db7f0cd13c5b8f68359581
Book Lailert <pl673@bath.ac.uk>
2022-04-27 17:51:29 +0100

    Added `putTextCentred()` to `MyText` for drawing text that is centred at a coordinate.

--- src/sleepAppGUI/interaction/MyText.java
+++ src/sleepAppGUI/interaction/MyText.java
@@ -8,9 +8,11 @@ public class MyText extends VObject
     public final static int defaultStyle = Font.PLAIN;
 
     private String text;
-    private String font = defaultFont;
+    private String fontName = defaultFont;
     private Color colour = Color.white;
     private int style = defaultStyle;
+
+    private boolean centre = false;
 
     public MyText(Page page, int[] coordinates1, int[] coordinates2, String text)
     {
@@ -29,9 +31,18 @@ public class MyText extends VObject
     {
         super(page, coordinates1, coordinates2, true);
         this.text = text;
-        this.font = fontName;
+        this.fontName = fontName;
         this.style = style;
         this.colour = textColour;
+    }
+
+    public MyText(Page page, int[] coordinates1, int[] coordinates2, String text, Color textColour, String fontName, int style, boolean centre) {
+        super(page, coordinates1, coordinates2, true);
+        this.text = text;
+        this.fontName = fontName;
+        this.style = style;
+        this.colour = textColour;
+        this.centre = centre;
     }
 
     public static MyText putText(Page page, int[] coordinates1, int[] size, String text, Color textColour, String fontName, int style) {
@@ -48,12 +59,28 @@ public class MyText extends VObject
         return putText(page, coordinates1, size, text, textColour, defaultFont, defaultStyle);
     }
 
+    public static MyText putTextCentred(Page page, int[] coordinateCentre, int[] size, String text, Color textColour, String fontName, int style) {
+        final int[] coordinates2 = new int[] {coordinateCentre[0] + size[0], coordinateCentre[1] + size[1]};
+
+        return new MyText(page, coordinateCentre, coordinates2, text, textColour, fontName, style, true);
+    }
+    public static MyText putTextCentred(Page page, int[] coordinateCentre, int[] size, String text, Color textColour) {
+        return putTextCentred(page, coordinateCentre, size, text, textColour, defaultFont, defaultStyle);
+    }
+
     @Override
     public void paint(Graphics g)
     {
-        g.setFont(new Font(font, style ,corner2[1] - corner1[1]));
-        g.setColor(colour);
-        g.drawString(text, corner1[0], corner1[1]);
+        Font font = new Font(this.fontName, this.style, this.corner2[1] - this.corner1[1]);
+        int[] topLeft = this.corner1.clone();
+        if (this.centre) {
+            FontMetrics fontMetrics = g.getFontMetrics(font);
+            int renderedWidth = fontMetrics.stringWidth(this.text);
+            topLeft[0] -= renderedWidth/2;
+        }
+        g.setFont(font);
+        g.setColor(this.colour);
+        g.drawString(this.text, topLeft[0], topLeft[1]);
     }
 
     public void setText(String string) { text = string; }
