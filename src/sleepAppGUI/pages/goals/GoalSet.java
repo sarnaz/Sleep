d4588803150b9500e8706f5ae40a2a513b6085f0
Book Lailert <pl673@bath.ac.uk>
2022-04-27 23:08:03 +0100

    BREAKING: `GoalSet` now requires override of a few more abstract functions for `pageTitle`, `unit` and `imageName`. `GoalSet` will now handle initialise of those on the page.

--- src/sleepAppGUI/pages/goals/GoalSet.java
+++ src/sleepAppGUI/pages/goals/GoalSet.java
@@ -7,12 +7,18 @@ import sleepAppGUI.visuals.ColourUtil;
 import java.awt.*;
 
 abstract public class GoalSet extends UIViewPage {
+
     @Override
     protected void setUp(Page page) {
 
         MyImage.putImage(page, new int[] {300, 20}, 200, "logo");
-        new MyRectangle(page, new int[] {80, 100}, new int[] {640, 370}, 50, Color.white);
+        new MyRectangle(page, new int[] {80, 100}, new int[] {640, 390}, 50, Color.white);
         new MyRectangle(page, new int[] {80, 100}, new int[] {640, 90}, 50, ColourUtil.foregroundColour);
+
+        MyImage.putImage(page, new int[] {250, 250}, 300, imageName());
+        MyText.putText(page, new int[] {110, 158}, 40, pageTitle(), Color.white, "Helvetica", Font.BOLD);
+        MyText.putText(page, new int[] {290, 235}, 30, "Goal:", Color.black, "Helvetica", Font.BOLD);
+        MyText.putText(page, new int[] {431, 235}, 30, unit(), Color.black, "Helvetica", Font.BOLD);
         MyButton backButton = new MyButton(page, "back", new int[]{240, 525}, new int[]{330, 555}, "back_button") {
             public void isClicked() {
                 GoalSet.this.push(new GoalPage());
@@ -20,4 +26,10 @@ abstract public class GoalSet extends UIViewPage {
             }
         };
     }
+
+    abstract protected String pageTitle();
+
+    abstract protected String unit();
+
+    abstract protected String imageName();
 }
