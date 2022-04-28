0ada5a03e02eefad1066823a35ec5eae8763c502
Book Lailert <pl673@bath.ac.uk>
2022-04-28 17:14:07 +0100

    Fix strava

--- src/sleepAppProcessing/Fitness.java
+++ src/sleepAppProcessing/Fitness.java
@@ -230,7 +230,7 @@ public class Fitness {
     public void addActivityData(int id, int day, int month, int year) {
 
         int totalActivityTime = 0;
-        long time = Date.valueOf(year+"-"+month+"-"+day).getTime();
+        long time = Date.valueOf(year+"-"+month+"-"+(day-1)).getTime()/1000;
 
         try {
 
