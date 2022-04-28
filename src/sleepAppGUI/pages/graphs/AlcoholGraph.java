ca9897511c411fe7eed421b8a26399ecbb5f1bd9
otj <oj302@bath.ac.uk>
2022-04-28 17:15:31 +0100

    Fixed issues with graphs

--- src/sleepAppGUI/pages/graphs/AlcoholGraph.java
+++ src/sleepAppGUI/pages/graphs/AlcoholGraph.java
@@ -24,8 +24,10 @@ public class AlcoholGraph extends GraphPage {
         MyText.putText(page, new int[] {110, 158}, 40, "Alcohol", Color.white, "Helvetica", Font.BOLD);
 
         MyBar alcohol_bar = new MyBar(page, new int[] {80,200}, new int[] {380,450});
+        alcohol_bar.setGraphScale(10);
         MyScatter alcohol_scatter = new MyScatter(page, new int[] {400,200}, new int[] {700,450});
         calendar = Calendar.getInstance();
+        System.out.println("IMPORTANT!!! \n\n\n\n\n\n\n\n\n");
         for (int i = 0; i < 5; i++) {
             int year=calendar.get(Calendar.YEAR);
             int month=calendar.get(Calendar.MONTH)+1;
@@ -34,14 +36,16 @@ public class AlcoholGraph extends GraphPage {
             if (data[1][0]!=null){
                 int j = (int) data[1][0];
                 double d = 1.0*j;
-                alcohol_bar.addPoint(dayOfWeek[day%7],d);
+                alcohol_bar.addPoint(dayOfWeek[7 - (day % 7)],d);
                 if (data[1][3]!=null){
                     int k = (int) data[1][3];
                     double e = 1.0*k;
                     alcohol_scatter.addPoint(d,e);
+                    System.out.println("d: "+d+" e: "+e);
                 }
             }
             calendar.add(Calendar.DATE,-1);
         }
+        alcohol_scatter.setGraphScale(10, 10);
     }
 }
